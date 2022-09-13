8b5197789a55b11d638220f12a13a9e8a7e1b7a6
Rusydy <rusydymuhiddin@gmail.com>
2022-09-13 10:41:38 +0700

    ADD: global variables

---
+++ global_vars.js
@@ -0,0 +1,13 @@
+// exports this variables to other files
+exports.invisible_border_color = ['#ffffff', '#ffffff', '#ffffff', '#ffffff'];
+exports.invisible_text_color = '#ffffff';
+exports.csd_file_name = 'csd.pdf';
+exports.invoice_file_name = 'invoice.pdf';
+exports.fonts = {
+	Roboto: {
+		normal: 'fonts/Roboto-Regular.ttf',
+		bold: 'fonts/Roboto-Medium.ttf',
+		italics: 'fonts/Roboto-Italic.ttf',
+		bolditalics: 'fonts/Roboto-MediumItalic.ttf'
+	}
+};
